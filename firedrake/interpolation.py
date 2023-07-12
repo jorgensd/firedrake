c0543b87a1ddfe8ce56ad78766067d5ae103bcb3
Reuben Nixon-Hill <ReubenHill@users.noreply.github.com>
2023-07-12 10:29:35 +0100

    Make expression check more permissive
    We need to allow firedrake.vector.Vector for example for adjoint to work

--- firedrake/interpolation.py
+++ firedrake/interpolation.py
@@ -160,8 +160,8 @@ class Interpolator(object):
 
         if self.nargs:
             function, = function
-            if not isinstance(function, ufl.Coefficient):
-                raise ValueError("The expression had arguments: we therefore need to be given a Function (not an expression) to interpolate!")
+            if not hasattr(function, "dat"):
+                raise ValueError("The expression had arguments: we therefore need to be given a function (not an expression) to interpolate!")
             if not self.vom_onto_vom:
                 if transpose:
                     mul = assembled_interpolator.handle.multTranspose
